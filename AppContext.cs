b7692cef47b8e8a2cce43433849ff3fd3e866ed2
MagHErmit <->
2020-09-20 12:34:01 +1000

    Add db context

---
+++ AppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_shell_dotnetcore.Models;
+
+namespace web_shell_dotnetcore
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<PowerShellCM> PSCommands { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=shellshistorydb;Trusted_Connection=True;");
+        }
+    }
+}
