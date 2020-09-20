eb082a57d89a0d3ebb2f384884a5ae23065d109c
MagHErmit <->
2020-09-20 12:33:10 +1000

    Add Command model

---
+++ Models/CommandModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_shell_dotnetcore.Models
+{
+    public abstract class CommandModel
+    {
+        public int Id { get; set; }
+        public string Command { get; set; }
+    }
+}
---
+++ Models/PowerShellCM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_shell_dotnetcore.Models
+{
+    public class PowerShellCM : CommandModel
+    {
+
+    }
+}
